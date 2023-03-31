a8d7c3d292d468d12ae06e99741d7be7d3e76940
“NicolasPeruchot” <nicolas.peruchot@gmail.com>
2023-03-31 11:21:18 +0200

    6.2.4

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -4,6 +4,8 @@ from mesa.time import RandomActivation
 from communication.agent.CommunicatingAgent import CommunicatingAgent
 from communication.message.MessageService import MessageService
 from communication.preferences.Preferences import Preferences
+from communication.message.Message import Message
+from communication.message.MessagePerformative import MessagePerformative
 
 from communication.preferences.CriterionName import CriterionName
 from communication.preferences.CriterionValue import CriterionValue
@@ -12,9 +14,9 @@ from communication.preferences.Value import Value
 
 
 class ArgumentAgent(CommunicatingAgent):
-    def __init__(self, unique_id, model, name, List_items):
+    def __init__(self, unique_id, model, name, list_items):
         super().__init__(unique_id, model, name)
-        self.preference = self.generate_preferences(List_items)
+        self.preference = self.generate_preferences(list_items)
 
     def step(self):
         super().step()
@@ -28,9 +30,10 @@ class ArgumentAgent(CommunicatingAgent):
         """
         pref = Preferences()
         for item in List_items.keys():
-            pref.add_criterion_value(
-                CriterionValue(item, List_items[item][0], List_items[item][1])
-            )
+            for criterion in List_items[item].keys():
+                pref.add_criterion_value(
+                    CriterionValue(item, criterion, List_items[item][criterion])
+                )
         return pref
 
 
@@ -38,63 +41,79 @@ class ArgumentModel(Model):
     def __init__(self):
         self.schedule = RandomActivation(self)
         self.__messages_service = MessageService(self.schedule)
+        diesel_engine = Item("Diesel Engine", "A super cool diesel engine")
+        electric_engine = Item("Electric Engine", "A very quiet engine")
+        A1_preferences = {
+            diesel_engine: {
+                CriterionName.PRODUCTION_COST: Value.VERY_GOOD,
+                CriterionName.CONSUMPTION: Value.GOOD,
+                CriterionName.DURABILITY: Value.VERY_GOOD,
+                CriterionName.ENVIRONMENT_IMPACT: Value.BAD,
+                CriterionName.NOISE: Value.AVERAGE,
+            },
+            electric_engine: {
+                CriterionName.PRODUCTION_COST: Value.AVERAGE,
+                CriterionName.CONSUMPTION: Value.BAD,
+                CriterionName.DURABILITY: Value.GOOD,
+                CriterionName.ENVIRONMENT_IMPACT: Value.VERY_GOOD,
+                CriterionName.NOISE: Value.VERY_GOOD,
+            },
+        }
 
-        # To be completed
-        #
-        # a = ArgumentAgent ( id , " agent_name ")
-        # a . generate_preferences ( preferences )
-        # self . schedule . add ( a )
-        # ...
+        A2_preferences = {
+            diesel_engine: {
+                CriterionName.PRODUCTION_COST: Value.GOOD,
+                CriterionName.CONSUMPTION: Value.AVERAGE,
+                CriterionName.DURABILITY: Value.GOOD,
+                CriterionName.ENVIRONMENT_IMPACT: Value.BAD,
+                CriterionName.NOISE: Value.BAD,
+            },
+            electric_engine: {
+                CriterionName.PRODUCTION_COST: Value.GOOD,
+                CriterionName.CONSUMPTION: Value.AVERAGE,
+                CriterionName.DURABILITY: Value.AVERAGE,
+                CriterionName.ENVIRONMENT_IMPACT: Value.VERY_GOOD,
+                CriterionName.NOISE: Value.VERY_GOOD,
+            },
+        }
+
+        self.A1 = ArgumentAgent(
+            unique_id=1,
+            model=self,
+            name="Nicolas",
+            list_items=A1_preferences,
+        )
+        self.A2 = ArgumentAgent(
+            unique_id=2,
+            model=self,
+            name="Thomas",
+            list_items=A2_preferences,
+        )
+        self.schedule.add(self.A1)
+        self.schedule.add(self.A2)
         self.running = True
 
     def step(self):
+        message = Message(
+            "Nicolas",
+            "Thomas",
+            message_performative=MessagePerformative.PROPOSE,
+            content="Chaud ?",
+        )
+
+        self.A1.send_message(message)
+
+        message = Message(
+            "Thomas",
+            "Nicolas",
+            message_performative=MessagePerformative.ACCEPT,
+            content="Ouais go",
+        )
+        self.A2.send_message(message)
         self.__messages_service.dispatch_messages()
         self.schedule.step()
 
 
-if __name__ == " __main__ ":
+if __name__ == "__main__":
     argument_model = ArgumentModel()
-
-    diesel_engine = Item("Diesel Engine", "A super cool diesel engine")
-    electric_engine = Item("Electric Engine", "A very quiet engine")
-
-    A1_preferences = {
-        diesel_engine: {
-            CriterionName.PRODUCTION_COST: Value.VERY_GOOD,
-            CriterionName.CONSUMPTION: Value.GOOD,
-            CriterionName.DURABILITY: Value.VERY_GOOD,
-            CriterionName.ENVIRONMENT_IMPACT: Value.BAD,
-            CriterionName.NOISE: Value.AVERAGE,
-        },
-        electric_engine: {
-            CriterionName.PRODUCTION_COST: Value.AVERAGE,
-            CriterionName.CONSUMPTION: Value.BAD,
-            CriterionName.DURABILITY: Value.GOOD,
-            CriterionName.ENVIRONMENT_IMPACT: Value.VERY_GOOD,
-            CriterionName.NOISE: Value.VERY_GOOD,
-        },
-    }
-
-    A2_preferences = {
-        diesel_engine: {
-            CriterionName.PRODUCTION_COST: Value.GOOD,
-            CriterionName.CONSUMPTION: Value.AVERAGE,
-            CriterionName.DURABILITY: Value.GOOD,
-            CriterionName.ENVIRONMENT_IMPACT: Value.BAD,
-            CriterionName.NOISE: Value.BAD,
-        },
-        electric_engine: {
-            CriterionName.PRODUCTION_COST: Value.GOOD,
-            CriterionName.CONSUMPTION: Value.AVERAGE,
-            CriterionName.DURABILITY: Value.AVERAGE,
-            CriterionName.ENVIRONMENT_IMPACT: Value.VERY_GOOD,
-            CriterionName.NOISE: Value.VERY_GOOD,
-        },
-    }
-
-    A1 = ArgumentAgent(
-        unique_id=1, model=argument_model, name="Nicolas", preferences=A1_preferences
-    )
-    A2 = ArgumentAgent(
-        unique_id=2, model=argument_model, name="Thomas", preferences=A2_preferences
-    )
+    argument_model.step()
